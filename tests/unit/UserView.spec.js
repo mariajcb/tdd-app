41e7f1349791f392d92df146bf5decc594823a3a
Maria Bogomaz <bogomazm@gmail.com>
2019-06-13 15:48:17 -0600

    refactor

--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -4,18 +4,26 @@ import UserSearchForm from '@/components/UserSearchForm'
 import UserProfile from '@/components/UserProfile'
 
 describe('UserView', () => {
+  const build = () => {
+    const wrapper = shallowMount(UserView)
+
+    return {
+      wrapper,
+      const userSearchForm: () => wrapper.find(UserSearchForm),
+      const userProfile: () => wrapper.find(UserProfile)
+    }
+  }
   it('renders the component', () => {
     //arrange
-    const wrapper = shallowMount(UserView)
+    const { wrapper } = build()
     //assert
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('renders main child components', () => {
-    const wrapper = shallowMount(UserView)
-    const userSearchForm = wrapper.find(UserSearchForm)
-    const userProfile = wrapper.find(UserProfile)
-
+    //arrange
+    const { userSearchForm, userProfile} = build()
+    //assert
     expect(userSearchForm.exists()).toBe(true)
     expect(userProfile.exists()).toBe(true)
   })
