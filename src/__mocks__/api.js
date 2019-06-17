0f754a31e77eaa035bc8a9fbbfb030fd3cb67867
Maria Bogomaz <bogomazm@gmail.com>
2019-06-17 16:41:32 -0600

    implements actions test

---
+++ src/__mocks__/api.js
@@ -0,0 +1,6 @@
+
+import userFixture from '../../tests/unit/fixtures/user'
+
+export default {
+  searchUser: jest.fn().mockResolvedValue(userFixture)
+}
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,15 @@
-// import userFixture from '../../../tests/unit/fixtures/user'
-//
-// export default {
-//   SEARCH_USER: jest.fn().mockResolvedValue(userFixture)
-// }
+import api from '@/api'
+
+export default {
+  SEARCH_USER({ commit }, { username }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const user = await api.searchUser(username)
+        commit('SET_USER', user)
+        resolve(user)
+      } catch(error) {
+        reject(error)
+      }
+    })
+  }
+}
